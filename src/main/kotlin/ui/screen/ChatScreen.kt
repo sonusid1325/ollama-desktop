744ba3431bf7897aaa098e198ebbc7de2556d45f
ソヌ <sonusid1325@gmail.com>
2025-02-12 23:19:40 +0530

    UI

--- src/main/kotlin/ui/screen/ChatScreen.kt
+++ src/main/kotlin/ui/screen/ChatScreen.kt
@@ -4,13 +4,25 @@ package ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -20,6 +32,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ChatScreen() {
     val logo = "drawable/logo.svg"
+    var prompt by remember { mutableStateOf("") }
     var messages = remember { mutableListOf<String>() }
     Scaffold(
         topBar = {
@@ -33,8 +46,28 @@ fun ChatScreen() {
                         )
                     }
                 },
-                title = { Text("Ollama") }
+                title = { Text("Ollama") },
             )
+        },
+        bottomBar = {
+            Row(Modifier.fillMaxWidth().padding(20.dp).padding(horizontal = 30.dp), verticalAlignment = Alignment.CenterVertically) {
+                OutlinedTextField(
+                    value = prompt,
+                    onValueChange = { newText -> prompt = newText },
+                    label = { Text("Enter Text") },
+                    shape = CircleShape,
+                    singleLine = true,
+                    modifier = Modifier.weight(1f),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = MaterialTheme.colorScheme.outline,
+                        focusedBorderColor = MaterialTheme.colorScheme.outline
+                    )
+                )
+                Spacer(Modifier.width(10.dp))
+                Button(onClick = {}, contentPadding = PaddingValues(0.dp), modifier = Modifier.size(50.dp)) {
+                    Icon(Icons.Rounded.KeyboardArrowRight, "enter")
+                }
+            }
         }
     ) { paddingValues ->
         if (messages.isEmpty()) {
